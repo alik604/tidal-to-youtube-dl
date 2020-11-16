857bd27b3aabc13587258403ee8d2477ad09f5ca
Alik604 <alik9807274@gmail.com>
2020-11-15 16:21:53 -0800

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from os import system
 import urllib
 session = tidalapi.Session()
 
-session.login('tom101@live.ca', 'look at me - xxxtentacion')
+session.login('tom101@live.ca', '')
 id1 = session.user.id
 fav = tidalapi.Favorites(session, id1)
 
@@ -11,8 +11,17 @@ fav = tidalapi.Favorites(session, id1)
 # print(fav)
 # print(fav.playlists())
 
+print([track.name for track in fav.tracks()])
+idx = 0
 for track in fav.tracks():
-    # print(track.name)
-    tmp = 'https://www.youtube.com/results?search_query=' + urllib.parse.quote(str(track.name))
-    system('start ' + tmp) # same as chome.exe http://facebook.com 
-
+    idx += 1 
+    if idx % 10 == 0:
+        _ = input()
+    for artist in track.artists:
+        name = artist.name
+        break # intensional
+    # print(name, track.name)
+    # print(track.id)
+    # break
+    tmp = 'https://www.youtube.com/results?search_query=' + urllib.parse.quote(str(track.name+" "+artist.name))
+    system('start ' + tmp) # same as chome.exe http://facebook.com
